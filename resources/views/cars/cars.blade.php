c35e990b582ca27c2cc5ac7fef848658b3d7cbe3
3mawy <m.3mawyy@gmail.com>
2022-05-24 18:02:32 +0200

    fix

---
+++ resources/views/cars/cars.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+        <div class="">
+          <h1>car details</h1>
+
+
+          <div class="row">
+            <div class="blog-post cccc">
+                <figure class="img">
+                    <img src="{{asset('img/blog-item-01.png')}}" alt="">
+                </figure>
+                <div class="blog-cont">
+                    <h3><a href="#">Vivamus ac vehicula dui</a></h3>
+                    <p class="text">Cras aliquet ligula dui, vitae fermentum velit tincidunt id. Praesent eu finibus nunc. Nulla in sagittis eros. Aliquam egestas augue.</p>
+                    <a href="car.html" class="main-btn">read more</a>
+                </div>
+            </div>
+            </div>
+        </div>
+@endsection
